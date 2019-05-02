02387802a6a1c39c1dd8879e838bb1d8cce29103
Bob <bobluppes@live.nl>
2019-05-02 09:46:49 +0200

    slow function

--- ClearSpeachMinimal.m
+++ ClearSpeachMinimal.m
@@ -7,19 +7,4 @@ Fn = Fs/2;
 n = length(original);
 m = 10000;
 
-O = fft(original);
-O = fftshift(O);
-t = linspace(0, (n/Fs), n);
-Omega = pi*[-1 : 2/n : 1-1/n];
-f = Omega*Fs/(2*pi);
-
-m = length(O);
-mask = [zeros(round(m/2), 1); ones(round(m/2) - 1, 1)];
-O = O .* mask;
-test = [zeros(m, 1); O];
-
-test = ifft(test);
-sound(test);
-
-figure;
-%plot(f, abs(test));
+improved = slow(original, Fs)
--- PSOLA/src/run.m
+++ PSOLA/src/run.m
@@ -1,3 +1,5 @@
+function WaveOut = slow(WaveIn, fs)
+
 %https://github.com/tiendung/voice-changer
 %--------------------------------------------------------------------------
 % main script to do pitch and time scale modification of speech signal
@@ -11,8 +13,8 @@ config.reconstruct          = 0;		%if true do low-band spectrum reconstruction
 config.displayPitchMarks    = 0;		%if true display pitch mark results
 config.playWavOut           = 1;		%if true send output waveform to speaker
 config.cutOffFreq           = 900;	%cut of frequency for lowpass filter
-config.fileIn               = '..\..\maleVoice.wav';		%input file full path and name
-config.fileOut              = '..\waves\syn.wav';		%output file full path and name
+%config.fileIn               = '..\..\maleVoice.wav';		%input file full path and name
+%config.fileOut              = '..\waves\syn.wav';		%output file full path and name
 
 %data contain analysis results
 global data;
@@ -20,18 +22,20 @@ data.waveOut = [];		%waveform after do pitch and time scale modification
 data.pitchMarks = [];	%pitch marks of input signal
 data.Candidates = [];	%pitch marks candidates
 
-[WaveIn, fs] = audioread(config.fileIn);	%read input signal from file
+%[WaveIn, fs] = audioread(config.fileIn);	%read input signal from file
 WaveIn = WaveIn - mean(WaveIn); 				%normalize input wave
 
 [LowPass] = LowPassFilter(WaveIn, fs, config.cutOffFreq); %low-pass filter for pre-processing
 PitchContour = PitchEstimation(LowPass, fs);							%pitch contour estimation
 PitchMarking(WaveIn, PitchContour, fs);										%do pitch marking and PSOLA
-audiowrite(config.fileOut, data.waveOut, fs);								%write output result to file
+%audiowrite(config.fileOut, data.waveOut, fs);								%write output result to file
 
-if config.playWavOut
-    sound(data.waveOut, fs);
-end
+WaveOut = waveOut;
 
-if config.displayPitchMarks
-    PlotPitchMarks(WaveIn, data.candidates, data.pitchMarks, PitchContour); %show the pitch marks
-end
+% if config.playWavOut
+%     sound(data.waveOut, fs);
+% end
+% 
+% if config.displayPitchMarks
+%     PlotPitchMarks(WaveIn, data.candidates, data.pitchMarks, PitchContour); %show the pitch marks
+% end
