c9b90cd69edd8dd697ad4b5d21bea52213b94e29
Bob <bobluppes@live.nl>
2019-05-06 11:05:43 +0200

    Formant function finished

--- formants.m
+++ formants.m
@@ -19,10 +19,40 @@ tf2 = bandpass(audio, [2050 2950], Fs);
 tf3 = bandpass(audio, [3050 3950], Fs);
 
 % Determine formant center frequencies
-m0 = 500;
-m1 = 1500;
-m2 = 2500;
-m3 = 3500;
+f = f(round(length(f)/2):end);
+f = resample(f, 1, 1000);
+
+F0 = fftshift(fft(tf0));
+F0 = F0(round(length(F0)/2):end);
+F0 = resample(abs(F0), 1, 1000);
+
+F1 = fftshift(fft(tf1));
+F1 = F1(round(length(F1)/2):end);
+F1 = resample(abs(F1), 1, 1000);
+
+F2 = fftshift(fft(tf2));
+F2 = F2(round(length(F2)/2):end);
+F2 = resample(abs(F2), 1, 1000);
+
+F3 = fftshift(fft(tf3));
+F3 = F3(round(length(F3)/2):end);
+F3 = resample(abs(F3), 1, 1000);
+
+m0 = 0;
+m1 = 0;
+m2 = 0;
+m3 = 0;
+for i = 1:length(F0)
+    m0 = m0 + (F0(i) * f(i));
+    m1 = m1 + (F1(i) * f(i));
+    m2 = m2 + (F2(i) * f(i));
+    m3 = m3 + (F3(i) * f(i));
+end
+m0 = m0 / (sum(abs(F0)));
+m1 = m1 / (sum(abs(F1)));
+m2 = m2 / (sum(abs(F2)));
+m3 = m3 / (sum(abs(F3)));
+
 
 % Determine formants around center frequencies
 f0 = bandpass(audio, [max((m0-450), 0) m0+450], Fs);
