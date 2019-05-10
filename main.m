035bd9d35ca65336f23bdaa264805395638f5fa2
Bob <bobluppes@live.nl>
2019-05-10 15:12:41 +0200

    Transient function

--- main.m
+++ main.m
@@ -1,3 +1,13 @@
 % audio input
 % noise input 
 
+clear all;
+close all;
+
+% Load audio signal
+[original,Fs] = audioread('Sounds/maleVoice.wav');
+
+improved = Transient(original, Fs, 5);
+
+sound(improved, Fs);
+
