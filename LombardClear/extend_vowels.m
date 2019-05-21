70d1977f1e57516ca9965aa7aaac92dec5c9c6d4
Ellen Riemens <e.h.j.riemens@student.tudelft.nl>
2019-05-21 12:31:28 +0200

    vowel recognition

--- LombardClear/extend_vowels.m
+++ LombardClear/extend_vowels.m
@@ -1,4 +1,5 @@
 function improved = extend_vowels(original, Fs, extension, sampleInt, thres, steps)
+% extension is 1/alpha 
 improved = [];
 alpha = 0.5;
 nleng = round(5*Fs/1000); %samples
@@ -7,6 +8,27 @@ wtype = 1; % Hamming
 deltamax = 1;%ms
 
 ipause = -1; % >=0 is plot for debug
+%% find vowels >> move to vowels_log_energy?
+sfn = vowels_log_energy(original, Fs);
+vowels = zeros(length(original),1);
+frame = round(length(original)/length(sfn));
+for i = 1:(length(sfn)-1)
+    if ((sfn(i)+6.5)<0)
+        vowels((i-1)*frame+1:(i)*frame) = 0;
+    else
+        vowels((i-1)*frame+1:(i)*frame) = 1;
+    end   
+end
+
+for i = 1:length(vowels)-1
+    if (vowels(i)-vowels(i+1) == -1)
+        % first index of vowel
+    elseif (vowels(i)-vowels(i+1) == 1)
+        % first index of consonant/last index of vowel
+    else
+        % same as before
+end
+
 for i = 0:(steps - 2)
     % Take timeframe
     x = original(round((i*sampleInt)+1):round((i+1)*sampleInt));
