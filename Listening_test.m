325b2bed4f850e604510fb83076bd15e2c8f2374
Bob <bobluppes@live.nl>
2019-06-14 15:16:24 +0200

    LUISTERTEST AF

--- Listening_test.m
+++ Listening_test.m
@@ -15,8 +15,14 @@ fluctuating_noise = [];
 for i = 1:length(non_fluctuating_noise)
     fluctuating_noise(i) = (sin(i/5000)) * noisepower*randn(1,1);
 end
-noise = non_fluctuating_noise(1:length(x));
+Pn = sqrt(sum(non_fluctuating_noise.^2));
+Pf = sqrt(sum(fluctuating_noise.^2));
+a = Pn ./ Pf;
+fluctuating_noise = fluctuating_noise .* a;
 
+[example,fs] = audioread('B1.wav');
+soundsc(example, fs);
+pause;
 % Vowel duration NF
 lombard_nf_audio = [];
 for i = 1:length(stretch)
@@ -39,6 +45,9 @@ for i = 1:length(stretch)
     close all;
 end
 
+[example,fs] = audioread('B2.wav');
+soundsc(example, fs);
+pause;
 % Tilting NF
 tilt_nf_audio = [];
 for i = 1:length(tilt)
@@ -61,6 +70,9 @@ for i = 1:length(tilt)
     close all;
 end
 
+[example,fs] = audioread('B3.wav');
+soundsc(example, fs);
+pause;
 % Compression NF
 compress_nf_audio = [];
 for i = 1:length(compression)
@@ -83,6 +95,9 @@ for i = 1:length(compression)
     close all;
 end
 
+[example,fs] = audioread('B4.wav');
+soundsc(example, fs);
+pause;
 % Vowel duration F
 lombard_f_audio = [];
 for i = 1:length(stretch)
@@ -105,6 +120,9 @@ for i = 1:length(stretch)
     close all;
 end
 
+[example,fs] = audioread('B5.wav');
+soundsc(example, fs);
+pause;
 % Tilting F
 tilt_f_audio = [];
 for i = 1:length(tilt)
@@ -127,6 +145,9 @@ for i = 1:length(tilt)
     close all;
 end
 
+[example,fs] = audioread('B6.wav');
+soundsc(example, fs);
+pause;
 % Compression F
 compress_f_audio = [];
 for i = 1:length(compression)
@@ -149,62 +170,156 @@ for i = 1:length(compression)
     close all;
 end
 
-return; 
+[music, fs] = audioread('Noorderlicht.wav');
+soundsc(music, fs);
+pause;
 
-% all test cases
-% 1 = vowelstretch
-% 2 = Spectral tilt
-% 3 = DRC
-% 2 noisetypes
-% unshuffled = [0 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 0 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3;
-%     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 ;
-%     0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 3 6 9 12 15 18 21 24 27 30 0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 3 6 9 12 15 18 21 24 27 30];
-%             
-% 
-% cols = size(unshuffled,2);
-% P = randperm(cols);
-% shuffled = unshuffled(:,P);
-load('Listening_test_variables.mat')
-
-i=1; 
-
-while i <= length(shuffled)
-    if shuffled(1,i) == 0
-        sound = x;
-    elseif shuffled(1,i) == 1
-        sound = extend_vowels(x, fs, shuffled(3,i));
-    elseif shuffled(1,i) == 2
-        sound = spectral_tilt(x, shuffled(3,i));
-    elseif shuffled(1,i) == 3
-        sound = compress(x, shuffled(3,i));
-    end
+[example,fs] = audioread('B4.wav');
+soundsc(example, fs);
+pause;
+% Vowel duration NF
+lombard_nf_audio2 = [];
+for i = 1:length(stretch)
+    j = randi([1 6]);
+    lombard_nf_audio2(i) = j;
+    [x,fs] = audioread(strcat('F1_b4_w',num2str(j),'_orig.wav'));
     
-    if (shuffled(2,i) == 1)
-        n = noise1;
-    elseif (shuffled(2,i) == 2)
-        n = noise2;   
-    end
+    enhanced = Lombard(x, fs, 10, stretch(i), 0, 0);
+    noise = non_fluctuating_noise(1:length(enhanced)); 
+    soundsc(enhanced+noise, fs);
     
-    soundsc(sound+n(1:length(sound)),fs);
-    i
-    figure
-    c = uicontrol('Style','pushbutton','String','Option 1','Callback', 'i=i-1');
-    uicontrol(c)
+    figure;
+    a = uicontrol('Style', 'text', 'String', 'VOWEL STRETCHING NF');
+    a.Position = [100 200 200 20];
+    b = uicontrol('Style', 'text', 'String', strcat(num2str(i),'/20'));
+    b.Position = [100 180 100 20];
+    c = uicontrol('Style','pushbutton','String','Repeat','Callback', 'soundsc(enhanced+noise, fs);');
+    c.Position = [100 140 50 20];
     pause;
+    close all;
+end
 
-%         function ButtonPushed(src,event)
-%             i=i-1;
-%         end
+[example,fs] = audioread('B5.wav');
+soundsc(example, fs);
+pause;
+% Tilting NF
+tilt_nf_audio2 = [];
+for i = 1:length(tilt)
+    j = randi([1 6]);
+    tilt_nf_audio2(i) = j;
+    [x,fs] = audioread(strcat('F1_b5_w',num2str(j),'_orig.wav'));
+    
+    enhanced = Lombard(x, fs, 10, 1, tilt(i), 0);
+    noise = non_fluctuating_noise(1:length(enhanced)); 
+    soundsc(enhanced+noise, fs);
+    
+    figure;
+    a = uicontrol('Style', 'text', 'String', 'SPECTRAL TILTING NF');
+    a.Position = [100 200 200 20];
+    b = uicontrol('Style', 'text', 'String', strcat(num2str(i),'/20'));
+    b.Position = [100 180 100 20];
+    c = uicontrol('Style','pushbutton','String','Repeat','Callback', 'soundsc(enhanced+noise, fs);');
+    c.Position = [100 140 50 20];
+    pause;
+    close all;
+end
 
-    i = i+1;
-%     noise = 0.07*randn(length(Il), 1);
-%     ext(i)
-%     siib_lombard(i) = SIIB_Gauss(Il, Il+noise, fs)
-%     soundsc(Il + noise, fs);
-%     pause;
-%     if (i ~= 0)
-%         duration = length(Il)/fs;
-%         answer = inputdlg(prompt);
-%         mod(i) = (str2num(answer{1})/duration + str2num(answer{2})/duration)/2;
-%     end
+[example,fs] = audioread('B6.wav');
+soundsc(example, fs);
+pause;
+% Compression NF
+compress_nf_audio2 = [];
+for i = 1:length(compression)
+    j = randi([1 6]);
+    compress_nf_audio2(i) = j;
+    [x,fs] = audioread(strcat('F1_b6_w',num2str(j),'_orig.wav'));
+    
+    enhanced = Lombard(x, fs, 10, 1, 0, compression(i));
+    noise = non_fluctuating_noise(1:length(enhanced)); 
+    soundsc(enhanced+noise, fs);
+    
+    figure;
+    a = uicontrol('Style', 'text', 'String', 'COMPRESSION NF');
+    a.Position = [100 200 200 20];
+    b = uicontrol('Style', 'text', 'String', strcat(num2str(i),'/20'));
+    b.Position = [100 180 100 20];
+    c = uicontrol('Style','pushbutton','String','Repeat','Callback', 'soundsc(enhanced+noise, fs);');
+    c.Position = [100 140 50 20];
+    pause;
+    close all;
+end
+
+[example,fs] = audioread('B1.wav');
+soundsc(example, fs);
+pause;
+% Vowel duration F
+lombard_f_audio2 = [];
+for i = 1:length(stretch)
+    j = randi([1 6]);
+    lombard_f_audio2(i) = j;
+    [x,fs] = audioread(strcat('F1_b1_w',num2str(j),'_orig.wav'));
+    
+    enhanced = Lombard(x, fs, 10, stretch(i), 0, 0);
+    noise = transpose(fluctuating_noise(1:length(enhanced))); 
+    soundsc(enhanced+noise, fs);
+    
+    figure;
+    a = uicontrol('Style', 'text', 'String', 'VOWEL STRETCHING F');
+    a.Position = [100 200 200 20];
+    b = uicontrol('Style', 'text', 'String', strcat(num2str(i),'/20'));
+    b.Position = [100 180 100 20];
+    c = uicontrol('Style','pushbutton','String','Repeat','Callback', 'soundsc(enhanced+noise, fs);');
+    c.Position = [100 140 50 20];
+    pause;
+    close all;
+end
+
+[example,fs] = audioread('B2.wav');
+soundsc(example, fs);
+pause;
+% Tilting F
+tilt_f_audio2 = [];
+for i = 1:length(tilt)
+    j = randi([1 6]);
+    tilt_f_audio2(i) = j;
+    [x,fs] = audioread(strcat('F1_b2_w',num2str(j),'_orig.wav'));
+    
+    enhanced = Lombard(x, fs, 10, 1, tilt(i), 0);
+    noise = transpose(fluctuating_noise(1:length(enhanced))); 
+    soundsc(enhanced+noise, fs);
+    
+    figure;
+    a = uicontrol('Style', 'text', 'String', 'SPECTRAL TILTING F');
+    a.Position = [100 200 200 20];
+    b = uicontrol('Style', 'text', 'String', strcat(num2str(i),'/20'));
+    b.Position = [100 180 100 20];
+    c = uicontrol('Style','pushbutton','String','Repeat','Callback', 'soundsc(enhanced+noise, fs);');
+    c.Position = [100 140 50 20];
+    pause;
+    close all;
+end
+
+[example,fs] = audioread('B3.wav');
+soundsc(example, fs);
+pause;
+% Compression F
+compress_f_audio2 = [];
+for i = 1:length(compression)
+    j = randi([1 6]);
+    compress_f_audio2(i) = j;
+    [x,fs] = audioread(strcat('F1_b3_w',num2str(j),'_orig.wav'));
+    
+    enhanced = Lombard(x, fs, 10, 1, 0, compression(i));
+    noise = transpose(fluctuating_noise(1:length(enhanced))); 
+    soundsc(enhanced+noise, fs);
+    
+    figure;
+    a = uicontrol('Style', 'text', 'String', 'COMPRESSION F');
+    a.Position = [100 200 200 20];
+    b = uicontrol('Style', 'text', 'String', strcat(num2str(i),'/20'));
+    b.Position = [100 180 100 20];
+    c = uicontrol('Style','pushbutton','String','Repeat','Callback', 'soundsc(enhanced+noise, fs);');
+    c.Position = [100 140 50 20];
+    pause;
+    close all;
 end
